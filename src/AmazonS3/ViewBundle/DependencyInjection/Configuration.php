a1298c3f13dbb0dc5b46ebec875d7ded6f7eb417
Joseph Bielawski <stloyd@gmail.com>
2012-11-09 12:24:19 +0100

    Add basic configuration

---
+++ src/AmazonS3/ViewBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AmazonS3\ViewBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('view');
+
+        $rootNode
+            ->children()
+                ->arrayNode('client_data')
+                    ->isRequired()
+                    ->children()
+                        ->scalarNode('key')->isRequired()->end()
+                        ->scalarNode('secret')->isRequired()->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
