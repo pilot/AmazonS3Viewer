7d5446d1a54ed02997fdf544b80fa224d7d88d4d
Joseph Bielawski <stloyd@gmail.com>
2012-11-09 13:21:16 +0100

    Fix lookup regex for finder and change default view

--- src/AmazonS3/ViewBundle/Controller/DefaultController.php
+++ src/AmazonS3/ViewBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
         $amazon->registerStreamWrapper('s3');
 
         $finder = new Finder();
-        $finder->name('images*')->size('< 100K')->date('since 1 day ago');
+        $finder->name('*-thumb.jpg')->size('< 100K')->date('since 1 day ago');
 
         return $this->render('ViewBundle:Default:index.html.twig', array(
             'images' => $finder->in('s3://bucket-name')
